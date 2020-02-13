39420684cc4bcd94db78b875691f436d0c557dce
Andy Zhao <andyzhao56@gmail.com>
2020-02-13 01:19:44 -0600

    added doctor/death visualization
    includes several todos

--- CoronaVirus.js
+++ CoronaVirus.js
@@ -1,14 +1,20 @@
 // Inspired by Nicky Case and Conway's Game of Life
-const states = ["", "👨🏻‍⚕️", "💀"];
+const states = ["", "🙍","🤢","💀","👨‍⚕️"];  // TODO add another human, as well as tree and fire
 const humanStates = ["👨‍💼","🙍‍♂️"];
-const gridSize = 12;
+const gridSize = 15;
 let grid = [];//lookup table of [x][y] = states
+const diseaseIndex = 2;
+const deathIndex = 3;
+const normalIndex = [1];
+const humanIndex = [1,4];
+const doctorIndex = 4;
 
 const humansPerFrame = 30;//amount of new tree created in each frame
+const doctorsPerFrame = 2;
 const diseaseProbability = 0.3;//chance to appear a new fire each frame
 
 function setup() {
-  frameRate(30);
+  frameRate(15);
   createCanvas(windowWidth, windowHeight);
   textSize(gridSize);
   stroke(200);
@@ -16,7 +22,7 @@ function setup() {
   for(let x = 0; x < width; x += gridSize){
     grid[x] = [];
     for(let y = 0; y < height; y += gridSize){
-      grid[x][y] = random() > 0.8 ? 1 : 0;
+      grid[x][y] = random() > 0.8 ? normalIndex[int(random(normalIndex.length))] : 0;
       //draw grid
       rect(x, y, gridSize, gridSize);
     }
@@ -30,18 +36,33 @@ function draw() {
   
   grid.forEach((column, indexX) => {
     column.forEach((state, indexY) => {
-      
       //draw emoji
-      if(state > 0) text(states[state], indexX, indexY + gridSize);
-      
-      let lastState = previousGrid[indexX][indexY];
-      if(lastState == 2){
-        propagate(2, [1], indexX, indexY);
-        //erase fire
+      if(state > 0) { 
+        text(states[state], indexX, indexY + gridSize);
+      }
+      fatality = random();
+      stateCounts = countSurroundingStates(previousGrid, indexX,indexY);
+      lastState = previousGrid[indexX][indexY];
+      if (lastState == deathIndex) {
+        // remove death
         grid[indexX][indexY] = 0;
       }
+      if(lastState == diseaseIndex){
+        if (fatality < 0.2) {
+          grid[indexX][indexY] = deathIndex;
+        } else {
+        propagate(diseaseIndex, humanIndex, indexX, indexY);
+        }
+      }
       
-      stateCounts = countSurroundingStates(indexX,indexY);
+      // immunization
+      if (lastState == doctorIndex) {
+        if (fatality > 0.95) {  // self-immunization
+          grid[indexX][indexY] = doctorIndex;
+        } else {
+          persistWithProbability(indexX, indexY, 0.4); // immunize surrounding
+        }
+      }
     });
   });
   
@@ -49,52 +70,59 @@ function draw() {
   for(let i = 0; i < humansPerFrame; i++){
     indexX = floor(random(width/gridSize)) * gridSize;
     indexY = floor(random(height/gridSize)) * gridSize;
-    grid[indexX][indexY] = 1;
+    grid[indexX][indexY] = normalIndex[int(random(normalIndex.length))];
+  }
+  
+  for (let i = 0; i < doctorsPerFrame; i++) {
+    indexX = floor(random(width/gridSize)) * gridSize;
+    indexY = floor(random(height/gridSize)) * gridSize;
+    grid[indexX][indexY] = doctorIndex;
   }
   
   //random disease
   if(random() > diseaseProbability){
     indexX = floor(random(width/gridSize)) * gridSize;
     indexY = floor(random(height/gridSize)) * gridSize;
-    grid[indexX][indexY] = 2;
+    grid[indexX][indexY] = diseaseIndex;
   }
+  
+  
 }
 
-function countSurroundingStates(indexX,indexY) {
+function countSurroundingStates(gridStates, indexX, indexY) {
   stateCount = [0]*states.length;
   if(indexX > 0) {
     //left
-    stateCount[grid[indexX-gridSize][indexY]] += 1;
+    stateCount[gridStates[indexX-gridSize][indexY]] += 1;
   }
   if(indexX < width - gridSize) {
     //right
-    stateCount[grid[indexX+gridSize][indexY]] += 1;
+    stateCount[gridStates[indexX+gridSize][indexY]] += 1;
   }
   if(indexY > 0) {
     //top
-    stateCount[grid[indexX][indexY-gridSize]] += 1;
+    stateCount[gridStates[indexX][indexY-gridSize]] += 1;
   }
   if(indexY < height - gridSize) {
     //bottom
-    stateCount[grid[indexX][indexY+gridSize]] += 1;
+    stateCount[gridStates[indexX][indexY+gridSize]] += 1;
   }
   if(indexX > 0 && indexY > 0) {
     //top left
-    stateCount[grid[indexX-gridSize][indexY-gridSize]] += 1;
+    stateCount[gridStates[indexX-gridSize][indexY-gridSize]] += 1;
   }
   if(indexX < width-gridSize && indexY > 0) {
     //top right
-    stateCount[grid[indexX+gridSize][indexY-gridSize]] += 1;
+    stateCount[gridStates[indexX+gridSize][indexY-gridSize]] += 1;
   }
   if(indexX > 0 && indexY < height - gridSize) {
     //bottom left
-    stateCount[grid[indexX-gridSize][indexY+gridSize]] += 1;
+    stateCount[gridStates[indexX-gridSize][indexY+gridSize]] += 1;
   }
   if(indexX < width-gridSize && indexY < height - gridSize) {
     //bottom right
-    stateCount[grid[indexX+gridSize][indexY+gridSize]] += 1;
+    stateCount[gridStates[indexX+gridSize][indexY+gridSize]] += 1;
   }
-
   return stateCount;
 }
 
@@ -131,4 +159,9 @@ function propagate(state, targets, indexX, indexY) {
     //bottom right
     grid[indexX+gridSize][indexY+gridSize] = state;
   }
+}
+
+// TODO add immunization to surrounding area
+function persistWithProbability(indexX, indexY, prob) {
+  
 }
